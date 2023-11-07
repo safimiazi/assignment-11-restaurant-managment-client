9da1cd0a252621c3af024daa67a9b046346a8c1c
safimiazi <mohibullamiazi@gmail.com>
2023-11-07 22:44:09 +0600

    jwt setup on the authstatechange

--- src/Components/AuthProvider/AuthProvider.jsx
+++ src/Components/AuthProvider/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { GithubAuthProvider, GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { createContext, useEffect, useState } from "react";
 import auth from "../firebase.config";
+import axios from "axios";
 
 export const AuthContext  = createContext(null)
 const AuthProvider = ({children}) => {
@@ -52,8 +53,22 @@ const  UserUpdate= (name,photo) => {
 
 useEffect(()=> {
     const unSubscribe = onAuthStateChanged(auth, (currentUser)=> {
+        const userEmail = currentUser?.email || user?.email;
+        const loggedUser = {email: userEmail}
+
         setUser(currentUser)
         setLoading(false)
+        if(currentUser){
+            axios.post('http://localhost:5000/api/v1/jwt',loggedUser, {withCredentials: true})
+            .then(res =>{
+                console.log("token response",res.data);
+            })
+        }else{
+            axios.post('http://localhost:5000/api/v1/logout', loggedUser,{withCredentials:true})
+            .then(res => {
+                console.log("token response", res.data);
+            })
+        }
     })
     return ()=> {
         return unSubscribe()
