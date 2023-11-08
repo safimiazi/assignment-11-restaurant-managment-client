0a488a94a44004dfeda873fd8ef5a04e5bd18244
safimiazi <mohibullamiazi@gmail.com>
2023-11-08 16:28:08 +0600

    fQ update

--- src/Components/FaQuestion/FaQuestion.jsx
+++ src/Components/FaQuestion/FaQuestion.jsx
@@ -1,6 +1,6 @@
 const FaQuestion = () => {
     return (
-        <section className="dark:bg-gray-800 dark:text-gray-100">
+        <section className="dark:bg-gray-800 max-w-screen-xl mx-auto dark:text-gray-100">
             <div className="container flex flex-col justify-center px-4 py-8 mx-auto md:p-8">
                 <h2 className="text-2xl font-semibold sm:text-4xl">Frequently Asked Questions</h2>
                 <p className="mt-4 mb-8 dark:text-gray-400">Hey ? STop !! Do you have any questions? Feel Free To Knock Us !!!!!!!</p>
